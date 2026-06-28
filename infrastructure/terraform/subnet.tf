4f4a139332b098f0d41e69fd8adc3a9380ec0f06
A00336144 <A00336144@student.tus.ie>
2026-06-28 12:08:38 +0100

    Add Terraform networking infrastructure

---
+++ infrastructure/terraform/subnet.tf
@@ -0,0 +1,10 @@
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.classroom_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "eu-west-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "Classroom-Public-Subnet"
+  }
+}
